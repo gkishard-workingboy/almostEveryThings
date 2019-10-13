2ca651989489feb1f70bcdf5d4cb1af50f95d765
gk's PC <304488689@qq.com>
2019-10-13 14:37:05 +0800

    会动的小车驻留程序v0.1

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Main_Form : Form
+    {
+        Form_Car form;
+        public Main_Form()
+        {
+            InitializeComponent();
+            form   = new Form_Car();
+        }
+
+        private void Main_Form_Load(object sender, EventArgs e)
+        {
+            this.ShowInTaskbar = false;
+            
+            form.ShowDialog();
+            if (MessageBox.Show("真的要退出吗", "最终确认", MessageBoxButtons.OKCancel).Equals(DialogResult.OK))
+            {
+                Application.Exit();
+            }
+                
+            
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            form.ShowDialog();
+            if (MessageBox.Show("真的要退出吗", "最终确认", MessageBoxButtons.OKCancel).Equals(DialogResult.OK))
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
